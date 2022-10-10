0e26ab5d6df10420bc6fa5896928be0734bb5a16
Taabeer <107323025+Taabeer@users.noreply.github.com>
2022-10-10 21:23:40 +0400

    Add files via upload

---
+++ clarkquestions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+
+let questions=[
+    {
+        numb:1,
+        question: "What object helped Emmanuel make friends at school?",
+        answer: "Football",
+        options:[
+            "Bicycle",
+            "Basketball",
+            "Football",
+            "Chocolate"
+        ]
+    },
+    {
+        numb:2,
+        question: "Emmanuel was born without a ______",
+        answer: "Leg",
+        options:[
+            "Mouth",
+            "Leg",
+            "Arm",
+            "Hand"
+        ]
+    },
+    {
+        numb:3,
+        question: "What Country is Emmanuel from?",
+        answer: "Ghana",
+        options:[
+            "Ghana",
+            "France",
+            "The UAE",
+            "Canada"
+        ]
+    },
+    {
+        numb:4,
+        question: "How did people with disabilities usually make money?",
+        answer: "Begging",
+        options:[
+            "Cooking",
+            "Playing football",
+            "Cleaning",
+            "Begging"
+        ]
+    },
+    {
+        numb:5,
+        question: "What is something that Emmanuel's mother said to him before she died?",
+        answer: "Both of these",
+        options:[
+            "Be respectful",
+            "Take care of your family",
+            "Both of these",
+            "None of these"
+        ]
+    },
+];
---
+++ questions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+
+let questions=[
+    {
+        numb:1,
+        question: "Why does Night feel unwanted?",
+        answer: "Night felt different",
+        options:[
+            "Night felt appreciated",
+            "Night felt different",
+            "Night felt loved",
+            "Night felt confident"
+        ]
+    },
+    {
+        numb:2,
+        question: "What does Sulwe mean?",
+        answer: "Star",
+        options:[
+            "Midnight",
+            "Sky",
+            "Star",
+            "Sunset"
+        ]
+    },
+    {
+        numb:3,
+        question: "Why did Day want Night to come back?",
+        answer: "She missed her",
+        options:[
+            "She wanted ice cream",
+            "She missed her",
+            "The night was too long",
+            "Life was better without Night"
+        ]
+    },
+    {
+        numb:4,
+        question: "What was the name of Sulwe's sister?",
+        answer: "Mich",
+        options:[
+            "Linda",
+            "Dawn",
+            "Mariah",
+            "Mich"
+        ]
+    },
+    {
+        numb:5,
+        question: "What did Sulwe do to make herself 'bright'?",
+        answer: "Used her mama's makeup",
+        options:[
+            "Used her mama's makeup",
+            "Poured lemon juice on herself",
+            "Rubbed pizza sauce on herself",
+            "Drank coffee"
+        ]
+    },
+];
